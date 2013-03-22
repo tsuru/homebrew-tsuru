9ddabb03dd50854d99a3a9bad27a898eeef44927
Francisco Souza <f@souza.cc>
2013-03-22 15:14:17 -0300

    crane: version 0.4

--- Formula/crane.rb
+++ Formula/crane.rb
@@ -1,7 +1,7 @@
 require 'formula'
 
 class Crane < Formula
-  url 'https://s3.amazonaws.com/tsuru/dist-src/crane-0.3.1.tar.gz'
+  url 'https://s3.amazonaws.com/tsuru/dist-src/crane-0.4.tar.gz'
   homepage 'http://tsuru.io'
   sha256 '5f7e51e909bb948db19d05cf14ec3ee40c6cff0d569cc84ef3c9aacc4286e11d'
 
