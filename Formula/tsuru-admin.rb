ec0df91b844fe58c38078892530adbc77af04d6c
Francisco Souza <f@souza.cc>
2013-03-27 17:46:21 -0300

    tsuru-admin: fix sha256 checksum

--- Formula/tsuru-admin.rb
+++ Formula/tsuru-admin.rb
@@ -3,7 +3,7 @@ require 'formula'
 class TsuruAdmin < Formula
   url 'https://s3.amazonaws.com/tsuru/dist-src/tsuru-admin-0.1.tar.gz'
   homepage 'http://tsuru.io'
-  sha256 '7a97528d168a3e93bbc9723d0e24cb01e8d78dc4f6200667498111e78a6e19af'
+  sha256 '1623aeee2188a1c76d71c35373f783f4470ededfeb36b96f72fbc71137e9fc72'
 
   depends_on 'go'
 
