16d4c57accfa6f1985a89fe2c137a0151e749aa1
Francisco Souza <fss@corp.globo.com>
2013-04-11 19:02:37 -0300

    tsuru-admin 0.1.3

--- Formula/tsuru-admin.rb
+++ Formula/tsuru-admin.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class TsuruAdmin < Formula
-  url 'https://s3.amazonaws.com/tsuru/dist-src/tsuru-admin-0.1.2.tar.gz'
+  url 'https://s3.amazonaws.com/tsuru/dist-src/tsuru-admin-0.1.3.tar.gz'
   homepage 'http://tsuru.io'
-  sha256 'e605368f9a86ba269b865251850a44794acdc5f327328ee89210dfabec78b97b'
+  sha256 '97c52807f82f9f3b003bc7bb0b627e26ef66dd9839f1d534c0f1da00f992f57e'
 
   depends_on 'go'
 
