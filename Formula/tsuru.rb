bb8eb5345ece4b0764115059aedda952af2d79d8
Francisco Souza <fss@corp.globo.com>
2013-08-15 10:22:35 -0300

    all: accept go 1.1.2
    I need to improve this :(

--- Formula/tsuru.rb
+++ Formula/tsuru.rb
@@ -8,7 +8,7 @@ class Tsuru < Formula
   depends_on 'go'
 
   def install
-    system "bash", "-c", "export GOVERSION=$(go version | cut -d' ' -f3) && [ \"x$GOVERSION\" = \"xdevel\" ] || [ \"x$GOVERSION\" = \"xgo1.1\" ] || [ \"x$GOVERSION\" = \"xgo1.1.1\" ] || (echo && echo \"ERROR: tsuru requires Go 1.1 or later, your version is $GOVERSION\" && exit 1)"
+    system "bash", "-c", "export GOVERSION=$(go version | cut -d' ' -f3) && [ \"x$GOVERSION\" = \"xdevel\" ] || [ \"x$GOVERSION\" = \"xgo1.1\" ] || [ \"x$GOVERSION\" = \"xgo1.1.1\" ] || [ \"x$GOVERSION\" = \"xgo1.1.2\" ] || (echo && echo \"ERROR: tsuru requires Go 1.1 or later, your version is $GOVERSION\" && exit 1)"
     system "bash", "-c", "GOPATH=\"$PWD\" go build -o tsuru github.com/globocom/tsuru/cmd/tsuru"
     bin.install "tsuru"
     bash_completion.install "src/github.com/globocom/tsuru/misc/bash-completion.d/tsuru"
