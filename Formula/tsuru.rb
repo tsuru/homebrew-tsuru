083a9e269491a2d0722c62b1a795a47c2362088f
Francisco Souza <f@souza.cc>
2014-03-11 10:17:25 -0300

    tsuru 0.8.9

--- Formula/tsuru.rb
+++ Formula/tsuru.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Tsuru < Formula
-  url 'https://s3.amazonaws.com/tsuru/dist-src/tsuru-0.8.8.tar.gz'
+  url 'https://s3.amazonaws.com/tsuru/dist-src/tsuru-0.8.9.tar.gz'
   homepage 'http://tsuru.io'
-  sha256 'b23268eee976d9dd4074254ab313331a64b74e9048d9223a817857190164f5b3'
+  sha256 '3ed5074e68eabeacf79cf186261d84f10fcbdabfc1ee6f57538968054d2f0654'
 
   depends_on 'go'
 
