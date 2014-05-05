215ea19916323d17f24f89f18710b9882e6a28e7
Andrews Medina <andrewsmedina@gmail.com>
2014-05-05 18:31:29 -0300

    tsuru-admin 0.3.0.

--- Formula/tsuru-admin.rb
+++ Formula/tsuru-admin.rb
@@ -1,15 +1,15 @@
 require 'formula'
 
 class TsuruAdmin < Formula
-  url 'https://s3.amazonaws.com/tsuru/dist-src/tsuru-admin-0.2.1.tar.gz'
+  url 'https://s3.amazonaws.com/tsuru/dist-src/tsuru-admin-0.3.0.tar.gz'
   homepage 'http://tsuru.io'
-  sha256 '15dfe07068697d2c421d44dea076026954be11cfe7c0c4d74642a2ecc892f15e'
+  sha256 'e740527dd8a2abb670f3fce39d8a6670ea2fcad059b5129553890640f71b2873'
 
   depends_on 'go'
 
   def install
     system "bash", "-c", "test $( go version|awk '{print \$3}' | sed 's/^[^0-9]*\\([0-9]\\)[^0-9]*\\([0-9]\\).*/\\1\\2/') -lt 11 && echo ERROR: tsuru-admin requires Go 1.1 or later, your version is: $(go version) && exit 1 || echo proceeding ..."
-    system "bash", "-c", "GOPATH=\"$PWD\" go build -o tsuru-admin github.com/globocom/tsuru/cmd/tsuru-admin"
+    system "bash", "-c", "GOPATH=\"$PWD\" go build -o tsuru-admin github.com/tsuru/tsuru/cmd/tsuru-admin"
     bin.install "tsuru-admin"
   end
 end
