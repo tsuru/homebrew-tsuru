c397e752175b8758a1d6de8473ee19c40d77ef85
Bernardo Heynemann <heynemann@gmail.com>
2014-06-26 16:47:26 -0300

    Proper version

--- Formula/gandalf.rb
+++ Formula/gandalf.rb
@@ -4,7 +4,7 @@ class Gandalf < Formula
   url 'https://s3.amazonaws.com/tsuru/dist-src/gandalf-0.4.0.tar.gz'
   homepage 'https://github.com/tsuru/gandalf'
   sha256 '73606eb0fcee7ccbe0926b29b2474da7c88678239128f1c100dc56722af3fbd1'
-  version '0.1.0'
+  version '0.4.0'
 
   depends_on 'go'
 
