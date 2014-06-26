ffecb3ba05b47affa3d529fc06e5adf8bcfff652
Bernardo Heynemann <heynemann@gmail.com>
2014-06-26 16:05:30 -0300

    Gandalf release 0.4.0

--- scripts/create-package.bash
+++ scripts/create-package.bash
@@ -14,7 +14,7 @@
 function usage {
 	echo "Usage:"
 	echo
-	echo "  % $0 tsuru-admin|tsuru|crane|all"
+	echo "  % $0 tsuru-admin|tsuru|crane|gandalf|all"
 	exit 1
 }
 
@@ -28,12 +28,15 @@ destination_dir="/tmp/dist-src"
 crane=0
 tsuru=0
 admin=0
+gandalf=0
 
 case $1 in
 	crane) crane=1;;
 	tsuru) tsuru=1;;
+	gandalf) gandalf=1;;
 	tsuru-admin) admin=1;;
 	all)
+		gandalf=1
 		crane=1
 		tsuru=1
 		admin=1;;
@@ -46,11 +49,29 @@ function get_version {
 	rm $1
 }
 
+function get_gandalf_version {
+	GOPATH=/tmp/tsuru-clients go build -o gandalf github.com/tsuru/gandalf/webserver
+	echo `./gandalf -version | awk '{print $3}' | sed -e 's/\.$//'`
+	rm gandalf
+}
+
 function download {
 	echo -n "Downloading source... "
 	mkdir -p /tmp/tsuru-clients/src /tmp/tsuru-clients/pkg
 	GOPATH=/tmp/tsuru-clients go get -d github.com/tsuru/tsuru/...
 	pushd $GOPATH/src/github.com/tsuru/tsuru > /dev/null 2>&1
+	echo -n "Restoring dependencies... "
+	GOPATH=/tmp/tsuru-clients godep restore ./...
+	popd > /dev/null 2>&1
+	echo "ok"
+}
+
+function download_gandalf {
+	echo -n "Downloading gandalf source... "
+	mkdir -p /tmp/tsuru-clients/src /tmp/tsuru-clients/pkg
+	GOPATH=/tmp/tsuru-clients go get -d github.com/tsuru/gandalf/...
+	pushd $GOPATH/src/github.com/tsuru/gandalf > /dev/null 2>&1
+	echo -n "Restoring dependencies... "
 	GOPATH=/tmp/tsuru-clients godep restore ./...
 	popd > /dev/null 2>&1
 	echo "ok"
@@ -67,7 +88,10 @@ echo -n "Creating \"$destination_dir\" directory... "
 mkdir -p $destination_dir
 echo "ok"
 
-download
+if [ $crane = 1 ] || [ $tsuru = 1 ] || [ $admin = 1 ]
+then
+	download
+fi
 
 if [ $crane = 1 ]
 then
@@ -90,11 +114,20 @@ then
 	echo $admin_version
 fi
 
+if [ $gandalf = 1 ]
+then
+	download_gandalf
+	echo -n "Determining gandalf version... "
+	gandalf_version=`get_gandalf_version`
+	echo $gandalf_version
+fi
+
 echo
 
 if [ $crane = 1 ]; then package ${destination_dir}/crane-${crane_version}.tar.gz; fi
 if [ $tsuru = 1 ]; then package ${destination_dir}/tsuru-${tsuru_version}.tar.gz; fi
 if [ $admin = 1 ]; then package ${destination_dir}/tsuru-admin-${admin_version}.tar.gz; fi
+if [ $gandalf = 1 ]; then package ${destination_dir}/gandalf-${gandalf_version}.tar.gz; fi
 
 rm -rf /tmp/tsuru-clients
 
