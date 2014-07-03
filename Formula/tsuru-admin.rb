6b140cdb8b25e2af97c85cad06320b152737c6e9
Francisco Souza <fss@corp.globo.com>
2014-07-03 19:36:54 -0300

    tsuru-admin 0.4.3
    Now with bash completion :)

--- Formula/tsuru-admin.rb
+++ Formula/tsuru-admin.rb
@@ -1,15 +1,16 @@
 require 'formula'
 
 class TsuruAdmin < Formula
-  url 'https://s3.amazonaws.com/tsuru/dist-src/tsuru-admin-0.4.2.tar.gz'
+  url 'https://s3.amazonaws.com/tsuru/dist-src/tsuru-admin-0.4.3.tar.gz'
   homepage 'http://tsuru.io'
-  sha256 'a6a468e811b67be964efc2e6989e3fb7da6011f21990d4e75c92fa3eef554aad'
+  sha256 'ce4d9f0c7f9618f95c0681e7ae38fe0d3aa57b154316e754d6583719456f7d54'
 
   depends_on 'go'
 
   def install
     system "bash", "-c", "test $( go version|awk '{print \$3}' | sed 's/^[^0-9]*\\([0-9]\\)[^0-9]*\\([0-9]\\).*/\\1\\2/') -lt 11 && echo ERROR: tsuru-admin requires Go 1.1 or later, your version is: $(go version) && exit 1 || echo proceeding ..."
-    system "bash", "-c", "GOPATH=\"$PWD\" go build -o tsuru-admin github.com/tsuru/tsuru/cmd/tsuru-admin"
+    system "bash", "-c", "GOPATH=\"$PWD\" go build -o tsuru-admin github.com/tsuru/tsuru-admin"
     bin.install "tsuru-admin"
+    bash_completion.install "src/github.com/tsuru/tsuru-admin/misc/bash-completion" => "tsuru-admin"
   end
 end
