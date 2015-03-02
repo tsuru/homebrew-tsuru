4c74b42a55f203bd1c76331402d83f384fb104c5
Francisco Souza <fss@corp.globo.com>
2015-03-02 18:59:22 -0300

    tsuru 0.15.1

--- Formula/tsuru.rb
+++ Formula/tsuru.rb
@@ -1,14 +1,14 @@
 require 'formula'
 
 class Tsuru < Formula
-  url 'https://s3.amazonaws.com/tsuru/dist-src/tsuru-0.15.0.tar.gz'
+  url 'https://s3.amazonaws.com/tsuru/dist-src/tsuru-0.15.1.tar.gz'
   homepage 'http://tsuru.io'
-  sha256 '8b20dce03fe0f6d87ce250c4d36f781f9bec18edfd04f0155c8123c92fa7f8e5'
+  sha256 'ef91c54706f5d23c8fc086a87dccc19c04555ddf11b1a189970870c2d5bdd7dd'
 
   depends_on 'go'
 
   def install
-    system "bash", "-c", "test $( go version|awk '{print \$3}' | sed 's/^[^0-9]*\\([0-9]\\)[^0-9]*\\([0-9]\\).*/\\1\\2/') -lt 11 && echo ERROR: tsuru requires Go 1.1 or later, your version is: $(go version) && exit 1 || echo proceeding ..."
+    system "bash", "-c", "test $( go version|awk '{print \$3}' | sed 's/^[^0-9]*\\([0-9]\\)[^0-9]*\\([0-9]\\).*/\\1\\2/') -lt 14 && echo ERROR: tsuru requires Go 1.4 or later, your version is: $(go version) && exit 1 || echo proceeding ..."
     system "bash", "-c", "GOPATH=\"$PWD\" go build -o tsuru github.com/tsuru/tsuru-client/tsuru"
     bin.install "tsuru"
     bash_completion.install "src/github.com/tsuru/tsuru-client/misc/bash-completion" => "tsuru"
