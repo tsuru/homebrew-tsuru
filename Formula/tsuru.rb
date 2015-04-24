2e32fe5b0c0ddf904d9da8183a288ede3043f205
Francisco Souza <fss@corp.globo.com>
2015-04-24 10:44:17 -0300

    tsuru: add manpage

--- Formula/tsuru.rb
+++ Formula/tsuru.rb
@@ -10,7 +10,9 @@ class Tsuru < Formula
   def install
     system "bash", "-c", "test $( go version|awk '{print \$3}' | sed 's/^[^0-9]*\\([0-9]\\)[^0-9]*\\([0-9]\\).*/\\1\\2/') -lt 14 && echo ERROR: tsuru requires Go 1.4 or later, your version is: $(go version) && exit 1 || echo proceeding ..."
     system "bash", "-c", "GOPATH=\"$PWD\" go build -o tsuru github.com/tsuru/tsuru-client/tsuru"
+    system "python", "src/github.com/tsuru/tsuru-client/docs/source/exts/man_pages.py"
     bin.install "tsuru"
+    man8.install "src/github.com/tsuru/tsuru-client/docs/source/exts/tsuru.8"
     bash_completion.install "src/github.com/tsuru/tsuru-client/misc/bash-completion" => "tsuru"
   end
 end
