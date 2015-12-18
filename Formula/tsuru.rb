a72f0fdad80397473d9b30a2596a420606a713a8
Francisco Souza <fss@corp.globo.com>
2015-12-18 14:46:18 -0200

    tsuru 0.18.1

--- Formula/tsuru.rb
+++ Formula/tsuru.rb
@@ -1,16 +1,11 @@
 require 'formula'
 
 class Tsuru < Formula
-  url "https://s3.amazonaws.com/tsuru/dist-src/tsuru-0.17.1.tar.gz"
+  url "https://s3.amazonaws.com/tsuru/dist-src/tsuru-0.18.1.tar.gz"
   homepage "https://tsuru.io"
-  sha256 "3aa64812c23267967f91087e412ae4952cc682d2b9783c2077517122bc36f70d"
+  sha256 "ff3b62c5e5cfbd74d000e92f95f38f61b5cb1cba57ecbaa077e76e071de1dfdc"
 
   depends_on 'go'
-
-  devel do
-    url "https://s3.amazonaws.com/tsuru/dist-src/tsuru-0.18.0.tar.gz"
-    sha256 "e69c92fa046f216b33051e60ae2ba018d70e8639f80ac7dd08aed9f3f9e41f74"
-  end
 
   def install
     system "bash", "-c", "test $( go version|awk '{print \$3}' | sed 's/^[^0-9]*\\([0-9]\\)[^0-9]*\\([0-9]\\).*/\\1\\2/') -lt 14 && echo ERROR: tsuru requires Go 1.4 or later, your version is: $(go version) && exit 1 || echo proceeding ..."
