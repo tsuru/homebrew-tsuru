48d04734aa51ef360767329b4814d9a197cce34d
Francisco Souza <fss@corp.globo.com>
2016-01-04 11:37:07 -0200

    crane 0.8.0

--- Formula/crane.rb
+++ Formula/crane.rb
@@ -1,14 +1,14 @@
 require 'formula'
 
 class Crane < Formula
-  url 'https://s3.amazonaws.com/tsuru/dist-src/crane-0.7.0.tar.gz'
-  homepage 'http://tsuru.io'
-  sha256 '229572fb8209469963ac6b5886d4e3821bc2668d7f516bffe8235de168673e59'
+  url 'https://s3.amazonaws.com/tsuru/dist-src/crane-0.8.0.tar.gz'
+  homepage 'https://tsuru.io'
+  sha256 'a3924bfad0668bd2f64c0a7ecbf2fb2e8824ebde0814ae5f7ecbb72b7132f775'
 
   depends_on 'go'
 
   def install
-    system "bash", "-c", "test $( go version|awk '{print \$3}' | sed 's/^[^0-9]*\\([0-9]\\)[^0-9]*\\([0-9]\\).*/\\1\\2/') -lt 11 && echo ERROR: crane requires Go 1.1 or later, your version is: $(go version) && exit 1 || echo proceeding ..."
+    system "bash", "-c", "test $( go version|awk '{print \$3}' | sed 's/^[^0-9]*\\([0-9]\\)[^0-9]*\\([0-9]\\).*/\\1\\2/') -lt 14 && echo ERROR: crane requires Go 1.4 or later, your version is: $(go version) && exit 1 || echo proceeding ..."
     system "bash", "-c", "GOPATH=\"$PWD\" go build -o crane github.com/tsuru/crane"
     bin.install "crane"
     bash_completion.install "src/github.com/tsuru/crane/misc/bash-completion" => "crane"
