37523a30f9c8584dd9f08237118a12ebc51a9719
Paulo Sousa <paulo.sousa@corp.globo.com>
2016-02-25 12:57:51 -0300

    tsuru: only installs man pages on stable release

--- Formula/tsuru.rb
+++ Formula/tsuru.rb
@@ -11,9 +11,11 @@ class Tsuru < Formula
     system "bash", "-c", "test -n \"$(go version | awk '{print \$3}' | perl -ne 'print if /go1\.[0-4](\.[0-9]+)?$/')\" && echo ERROR: tsuru requires Go 1.5 or later, your version is: $(go version) && exit 1 || echo proceeding ..."
     system "bash", "-c", "GOPATH=\"$PWD\" go build -o tsuru github.com/tsuru/tsuru-client/tsuru"
     ENV['TSURU_PATH'] = "./tsuru"
-    system "python", "src/github.com/tsuru/tsuru-client/docs/source/exts/man_pages.py"
     bin.install "tsuru"
-    man8.install "src/github.com/tsuru/tsuru-client/docs/source/exts/tsuru.8"
+    unless ENV.key?('TSURU_DEVEL')
+      system "python", "src/github.com/tsuru/tsuru-client/docs/source/exts/man_pages.py"
+      man8.install "src/github.com/tsuru/tsuru-client/docs/source/exts/tsuru.8"
+    end
     bash_completion.install "src/github.com/tsuru/tsuru-client/misc/bash-completion" => "tsuru"
     zsh_completion.install "src/github.com/tsuru/tsuru-client/misc/zsh-completion" => "tsuru"
   end
@@ -21,5 +23,7 @@ class Tsuru < Formula
   devel do
     url "https://s3.amazonaws.com/tsuru/dist-src/tsuru-1.0.0.tar.gz"
     sha256 "a33d8c72a3bc99205a4b75544fb103725e4d797dd19248efe66d75f124ddb5ba"
+    ENV['TSURU_DEVEL'] = "true"
   end
+
 end
