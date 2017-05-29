5cf30f00b610d98897c23010c82d0f139b5f5769
Paulo Sousa <paulo.sousa@corp.globo.com>
2017-05-29 15:30:12 -0300

    bump to 1.3.0-rc1

--- Formula/tsuru.rb
+++ Formula/tsuru.rb
@@ -9,6 +9,11 @@ class Tsuru < Formula
     bin.install "tsuru"
     bash_completion.install "misc/bash-completion" => "tsuru"
     zsh_completion.install "misc/zsh-completion" => "tsuru"
-    
+
+  end
+  devel do
+    url "https://github.com/tsuru/tsuru-client/releases/download/1.3.0-rc1/tsuru_1.3.0-rc1_macOS_amd64.tar.gz"
+    sha256 "7d783570a3e016ad0bd957905317e1bcd51c92c5be08cc1c5ed2fcf167d24222"
+    ENV['TSURU_DEVEL'] = "true"
   end
 end
