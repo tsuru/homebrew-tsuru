3e080f5136f9aa0c724f3a0879b72f628c221c11
André Carvalho <asantostc@gmail.com>
2017-07-27 10:16:03 -0300

    fix formula desc and homepage

--- Formula/tsuru.rb
+++ Formula/tsuru.rb
@@ -1,6 +1,6 @@
 class Tsuru < Formula
-  desc ""
-  homepage ""
+  desc "tsuru-client is a tsuru command line tool for application developers."
+  homepage "https://docs.tsuru.io/stable/"
   url "https://github.com/tsuru/tsuru-client/releases/download/1.3.0/tsuru_1.3.0_macOS_amd64.tar.gz"
   version "1.3.0"
   sha256 "12bca17651135f8e959348a4e87c2f346960f3b09e4cf412308409ae4b254b93"
