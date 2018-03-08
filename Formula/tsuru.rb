a8db87467a0da08135f22f1f79d0bd64590929e8
Guilherme Garnier <guilherme.garnier@gmail.com>
2018-03-08 15:52:35 -0300

    bump tsuru devel to 1.5.0-rc4

--- Formula/tsuru.rb
+++ Formula/tsuru.rb
@@ -11,9 +11,9 @@ class Tsuru < Formula
     zsh_completion.install "misc/zsh-completion" => "tsuru"
   end
   devel do
-    url "https://github.com/tsuru/tsuru-client/releases/download/1.5.0-rc3/tsuru_1.5.0-rc3_macOS_amd64.tar.gz"
-    version "1.5.0-rc3"
-    sha256 "b97ffe5acddff3ca9fc31f2b7827e9b7ecff190cff6cf3fa4659c335c2248b14"
+    url "https://github.com/tsuru/tsuru-client/releases/download/1.5.0-rc4/tsuru_1.5.0-rc4_macOS_amd64.tar.gz"
+    version "1.5.0-rc4"
+    sha256 "5143e98c7cd166f21dfa76fa82b45f77d4bea732c8c8667b1a10e856612f3f8f"
     ENV['TSURU_DEVEL'] = "true"
   end
 end
