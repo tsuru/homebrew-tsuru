f1f60f92b6735803661d900958a0caa48c76fdce
goreleaserbot <goreleaser@carlosbecker.com>
2019-07-16 17:48:46 -0300

    tsuru version 1.7.0

--- Formula/tsuru.rb
+++ Formula/tsuru.rb
@@ -1,21 +1,14 @@
 class Tsuru < Formula
   desc "tsuru-client is a tsuru command line tool for application developers."
   homepage "https://docs.tsuru.io/stable/"
-  url "https://github.com/tsuru/tsuru-client/releases/download/1.6.0/tsuru_1.6.0_macOS_amd64.tar.gz"
-  version "1.6.0"
-  sha256 "7b5a46a51ba06913249be6c6ac5396ea6c4b8613ef441e3eea3d290f18da8057"
+  url "https://github.com/tsuru/tsuru-client/releases/download/1.7.0/tsuru_1.7.0_macOS_amd64.tar.gz"
+  version "1.7.0"
+  sha256 "e6032cdcc3c29c05c62f0b9dc8d7b38cfba9e9383f1f8850c930a4fd9ec1e081"
 
   def install
     bin.install "tsuru"
     bash_completion.install "misc/bash-completion" => "tsuru"
     zsh_completion.install "misc/zsh-completion" => "tsuru"
-  end
-  
-  devel do
-    url "https://github.com/tsuru/tsuru-client/releases/download/1.7.0-rc1/tsuru_1.7.0-rc1_macOS_amd64.tar.gz"
-    version "1.7.0-rc1"
-    sha256 "bd9a5e13e802caf8b43557df9d5ef19aae4a581547d28b2b1b2ce4deb362dfae"
-    ENV['TSURU_DEVEL'] = "true"
   end
 
   test do
