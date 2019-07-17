3ee48266cb08fe7835c9a4fb58c10598a10692e6
Cezar Sa Espinola <cezarsa@gmail.com>
2019-07-17 10:56:33 -0300

    Add devel release pointing to stable version
    No surprises or errors for people using install --devel by default.

--- Formula/tsuru.rb
+++ Formula/tsuru.rb
@@ -11,6 +11,12 @@ class Tsuru < Formula
     zsh_completion.install "misc/zsh-completion" => "tsuru"
   end
 
+  devel do
+    url "https://github.com/tsuru/tsuru-client/releases/download/1.7.0/tsuru_1.7.0_macOS_amd64.tar.gz"
+    version "1.7.0"
+    sha256 "e6032cdcc3c29c05c62f0b9dc8d7b38cfba9e9383f1f8850c930a4fd9ec1e081"
+  end
+
   test do
     
   end
