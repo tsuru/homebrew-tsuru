244cc89855573d8b09cac948762c07af7865eb97
goreleaserbot <goreleaser@carlosbecker.com>
2020-12-08 11:14:43 -0300

    Brew formula update for tsuru version 1.9.0

--- Formula/tsuru.rb
+++ Formula/tsuru.rb
@@ -2,17 +2,16 @@
 class Tsuru < Formula
   desc "tsuru-client is a tsuru command line tool for application developers."
   homepage "https://docs.tsuru.io/stable/"
-  version "1.8.3"
+  version "1.9.0"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/tsuru/tsuru-client/releases/download/1.8.3/tsuru_1.8.3_macOS_amd64.tar.gz"
-    sha256 "be4ba0feca1669ffa650e809c56c81516e0f1d5ebbadb159707caed857997b0a"
-  elsif OS.linux?
-    if Hardware::CPU.intel?
-      url "https://github.com/tsuru/tsuru-client/releases/download/1.8.3/tsuru_1.8.3_linux_amd64.tar.gz"
-      sha256 "6ef056714d999c4f636b0ee76574ca931c0d2b0b0f96b889078ab8e1210522c5"
-    end
+    url "https://github.com/tsuru/tsuru-client/releases/download/1.9.0/tsuru_1.9.0_macOS_amd64.tar.gz"
+    sha256 "6b2bd97dc7075be11e157a6bcfe5fe3a6c7109cb0978c9b8c6820a94ec62795c"
+  end
+  if OS.linux? && Hardware::CPU.intel?
+    url "https://github.com/tsuru/tsuru-client/releases/download/1.9.0/tsuru_1.9.0_linux_amd64.tar.gz"
+    sha256 "90c43317e8c8806376ec5049a7b6197b912de64f52e9717a2a2ede1cf3d0af95"
   end
 
   def install
